63532203edb194714ea78993ac2552329f42c4ab
Maxim Sirotkin <d.zh2012@yandex.ru>
2018-12-03 01:02:43 +0300

    +EventData

---
+++ Photon/EventData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Photon
+{
+    public class EventData
+    {
+        public byte Code;
+        public Dictionary<byte, object> Parameters;
+
+        public object this[byte key]
+        {
+            get
+            {
+                Parameters.TryGetValue(key, out object result);
+
+                return result;
+            }
+            set
+            {
+                Parameters[key] = value;
+            }
+        }
+
+        //public override string ToString()
+        //{
+        //    return string.Format("Event {0}.", this.Code.ToString());
+        //}
+
+        //public string ToStringFull()
+        //{
+        //    return string.Format("Event {0}: {1}", Code, SupportClass.DictionaryToString(Parameters));
+        //}
+    }
+}
