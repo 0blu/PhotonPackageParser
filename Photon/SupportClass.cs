5b0d3146da07f0c06db558a68b5faca7bd7cccb7
Maxim Sirotkin <d.zh2012@yandex.ru>
2018-12-03 00:59:27 +0300

    +SupportClass

---
+++ Photon/SupportClass.cs
@@ -0,0 +1,30 @@
+﻿namespace Photon
+{
+    public class SupportClass
+    {
+        public static uint CalculateCrc(byte[] buffer, int length)
+        {
+            uint maxValue = uint.MaxValue;
+            uint num2 = 3988292384u;
+            for (int i = 0; i < length; i++)
+            {
+                byte b = buffer[i];
+                maxValue ^= b;
+                for (int j = 0; j < 8; j++)
+                {
+                    bool flag = (maxValue & 1u) > 0u;
+                    if (flag)
+                    {
+                        maxValue = (maxValue >> 1 ^ num2);
+                    }
+                    else
+                    {
+                        maxValue >>= 1;
+                    }
+                }
+            }
+
+            return maxValue;
+        }
+    }
+}
