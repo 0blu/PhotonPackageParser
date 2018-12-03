4f6103f33159268163d303d13b81caa3b1595a2b
Maxim Sirotkin <d.zh2012@yandex.ru>
2018-12-03 23:04:21 +0300

    Refactoring Protocol

--- Photon/Protocol.cs
+++ Photon/Protocol.cs
@@ -1,22 +1,13 @@
-﻿using System;
-
-namespace Photon
+﻿namespace Photon
 {
     public class Protocol
     {
-        private static readonly float[] memFloatBlock = new float[1];
-        private static readonly byte[] memDeserialize = new byte[4];
+        #region fields
+        private static readonly float[] memoryFloatBlock = new float[1];
+        private static readonly byte[] memoryDeserialize = new byte[4];
+        #endregion
 
-        public static void Serialize(short value, byte[] target, ref int targetOffset)
-        {
-            int num = targetOffset;
-            targetOffset = num + 1;
-            target[num] = (byte)(value >> 8);
-            num = targetOffset;
-            targetOffset = num + 1;
-            target[num] = (byte)value;
-        }
-
+        #region methods
         public static void Serialize(int value, byte[] target, ref int targetOffset)
         {
             int num = targetOffset;
@@ -31,27 +22,6 @@ namespace Photon
             num = targetOffset;
             targetOffset = num + 1;
             target[num] = (byte)value;
-        }
-
-        public static void Serialize(float value, byte[] target, ref int targetOffset)
-        {
-            float[] obj = Protocol.memFloatBlock;
-            lock (obj)
-            {
-                Protocol.memFloatBlock[0] = value;
-                Buffer.BlockCopy(Protocol.memFloatBlock, 0, target, targetOffset, 4);
-            }
-            bool isLittleEndian = BitConverter.IsLittleEndian;
-            if (isLittleEndian)
-            {
-                byte b = target[targetOffset];
-                byte b2 = target[targetOffset + 1];
-                target[targetOffset] = target[targetOffset + 3];
-                target[targetOffset + 1] = target[targetOffset + 2];
-                target[targetOffset + 2] = b2;
-                target[targetOffset + 3] = b;
-            }
-            targetOffset += 4;
         }
 
         public static void Deserialize(out int value, byte[] source, ref int offset)
@@ -79,44 +49,6 @@ namespace Photon
             offset = num + 1;
             value = (short)(b | source[num]);
         }
-
-        public static void Deserialize(out float value, byte[] source, ref int offset)
-        {
-            bool isLittleEndian = BitConverter.IsLittleEndian;
-            if (isLittleEndian)
-            {
-                byte[] obj = Protocol.memDeserialize;
-                lock (obj)
-                {
-                    byte[] array = Protocol.memDeserialize;
-                    byte[] array2 = array;
-                    int num = 3;
-                    int num2 = offset;
-                    offset = num2 + 1;
-                    array2[num] = source[num2];
-                    byte[] array3 = array;
-                    int num3 = 2;
-                    num2 = offset;
-                    offset = num2 + 1;
-                    array3[num3] = source[num2];
-                    byte[] array4 = array;
-                    int num4 = 1;
-                    num2 = offset;
-                    offset = num2 + 1;
-                    array4[num4] = source[num2];
-                    byte[] array5 = array;
-                    int num5 = 0;
-                    num2 = offset;
-                    offset = num2 + 1;
-                    array5[num5] = source[num2];
-                    value = BitConverter.ToSingle(array, 0);
-                }
-            }
-            else
-            {
-                value = BitConverter.ToSingle(source, offset);
-                offset += 4;
-            }
-        }
+        #endregion
     }
 }
