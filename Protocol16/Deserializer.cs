11698af9911f128e3e60ff9c8ea25ef00305a10f
Maxim Sirotkin <d.zh2012@yandex.ru>
2018-12-08 01:39:25 +0300

    Refactoring Deserializer

--- Protocol16/Deserializer.cs
+++ Protocol16/Deserializer.cs
@@ -2,49 +2,23 @@
 {
     public class Deserializer
     {
-        public static void Deserialize(out short value, byte[] source, ref int offset)
-        {
-            int num = offset;
-            offset = num + 1;
-            short b = (short)(source[num] << 8);
-            num = offset;
-            offset = num + 1;
-            value = (short)(b | source[num]);
-        }
-
         public static void Deserialize(out int value, byte[] source, ref int offset)
         {
-            int num = offset;
-            offset = num + 1;
-            int num2 = (int)source[num] << 24;
-            num = offset;
-            offset = num + 1;
-            int num3 = num2 | (int)source[num] << 16;
-            num = offset;
-            offset = num + 1;
-            int num4 = num3 | (int)source[num] << 8;
-            num = offset;
-            offset = num + 1;
-            value = (num4 | (int)source[num]);
+            int value1 = source[offset] << 24;
+            offset++;
+            int value2 = value1 | source[offset] << 16;
+            offset++;
+            int value3 = value2 | source[offset] << 8;
+            offset++;
+            value = (value3 | source[offset]);
         }
 
-        //public static void Deserialize(out int value, byte[] source, ref int offset)
-        //{
-        //    int v1 = source[offset] << 24;
-        //    offset++;
-        //    int v2 = v1 | source[offset] << 16;
-        //    offset++;
-        //    int v3 = v2 | source[offset] << 8;
-        //    offset++;
-        //    value = (v3 | source[offset]);
-        //}
-
-        //public static void Deserialize(out short value, byte[] source, ref int offset)
-        //{
-        //    byte byte1 = (byte)(source[offset] << 8);
-        //    offset++;
-        //    value = (short)(byte1 | source[offset]);
-        //    offset++;
-        //}
+        public static void Deserialize(out short value, byte[] source, ref int offset)
+        {
+            short value1 = (short)(source[offset] << 8);
+            offset++;
+            value = (short)(value1 | (short)source[offset]);
+            offset++;
+        }
     }
 }
