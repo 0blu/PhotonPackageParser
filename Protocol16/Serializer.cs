bcaaeb9b534690f821d9056cb394b8d3045dbf59
Maxim Sirotkin <d.zh2012@yandex.ru>
2018-12-08 01:39:34 +0300

    Refactoring Serializer

--- Protocol16/Serializer.cs
+++ Protocol16/Serializer.cs
@@ -2,32 +2,16 @@
 {
     public class Serializer
     {
-        //public static void Serialize(int value, byte[] target, ref int offset)
-        //{
-        //    target[offset] = (byte)(value >> 24);
-        //    offset++;
-        //    target[offset] = (byte)(value >> 16);
-        //    offset++;
-        //    target[offset] = (byte)(value >> 8);
-        //    offset++;
-        //    target[offset] = (byte)value;
-        //    offset++;
-        //}
-
-        public static void Serialize(int value, byte[] target, ref int targetOffset)
+        public static void Serialize(int value, byte[] target, ref int offset)
         {
-            int num = targetOffset;
-            targetOffset = num + 1;
-            target[num] = (byte)(value >> 24);
-            num = targetOffset;
-            targetOffset = num + 1;
-            target[num] = (byte)(value >> 16);
-            num = targetOffset;
-            targetOffset = num + 1;
-            target[num] = (byte)(value >> 8);
-            num = targetOffset;
-            targetOffset = num + 1;
-            target[num] = (byte)value;
+            target[offset] = (byte)(value >> 24);
+            offset++;
+            target[offset] = (byte)(value >> 16);
+            offset++;
+            target[offset] = (byte)(value >> 8);
+            offset++;
+            target[offset] = (byte)value;
+            offset++;
         }
     }
 }
