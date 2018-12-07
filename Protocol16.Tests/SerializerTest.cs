bafb7cea8e7c60536e44714d893b28b0e2270ab6
Maxim Sirotkin <d.zh2012@yandex.ru>
2018-12-08 01:34:43 +0300

    +tests

---
+++ Protocol16.Tests/SerializerTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Protocol16.Tests
+{
+    [TestFixture]
+    public class SerializerTest
+    {
+        [Test]
+        public void SerializeInteger()
+        {
+            byte[] resultBuffer = new byte[]
+            {
+                0, 0, 4, 210
+            };
+            byte[] buffer = new byte[4];
+            int offset = 0;
+
+            Serializer.Serialize(1234, buffer, ref offset);
+
+            Assert.AreEqual(buffer, resultBuffer);
+        }
+    }
+}
